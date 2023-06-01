ca162646e3479878fd34030ae110e1712181082e
Andre Nunes <andred_nunes@hotmail.com>
2023-06-01 15:15:42 -0300

    added remove_node() function

--- linked_list.py
+++ linked_list.py
@@ -1,26 +1,45 @@
 class Node:
-    def __init__(self, value, link_node = None )-> None:
+    def __init__(self, value, next_node = None )-> None:
         self.value = value
-        self.link_node = link_node
+        self.next_node = next_node
     
     def get_value(self):
         return self.value
 
-    def get_link_node(self):
-        return self.link_node
+    def get_next_node(self):
+        return self.next_node
 
-    def set_link_node(self, link_node):
-        self.link_node = link_node
+    def set_next_node(self, next_node):
+        self.next_node = next_node
 
-yacko = Node('likes to yak')
-wacko =  Node('has a penchant for hoarding snacks')
-dot = Node('enjoys spending time in movie lots')
+class LinkedList:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
 
-yacko.set_link_node(dot)
-dot.set_link_node(wacko)
-
-dots_data = yacko.get_link_node().get_value()
-wackos_data = dot.get_link_node().get_value()
-
-print(dots_data)
-print(wackos_data)
+    def get_head_node(self):
+        return self.head_node
+    
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_node)
+        
+        self.head_node = new_node
+    def stringify_list(self):
+        string_list = ''
+        current_node = self.head_node
+        string_list += f'{current_node.get_value()}\n'
+        while current_node.get_next_node() != None:
+            current_node = current_node.get_next_node()
+            string_list += f'{current_node.get_value()}\n'
+        
+        return string_list
+    
+    def remove_node(self, value_to_remove):
+        current_node = self.get_head_node()
+        if current_node == value_to_remove:
+            self.head_node = current_node.get_next_value()
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+print(ll.stringify_list())

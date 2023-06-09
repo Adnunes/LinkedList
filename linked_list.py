c7c4bf3e77ebe5d790ee0e294aba3acc92221e2a
Andre Nunes <andred_nunes@hotmail.com>
2023-06-09 13:08:08 -0300

    Added Doubly Linked List

--- linked_list.py
+++ linked_list.py
@@ -1,4 +1,5 @@
 class Node:
+
     def __init__(self, value, next_node = None )-> None:
         self.value = value
         self.next_node = next_node
@@ -13,6 +14,7 @@ class Node:
         self.next_node = next_node
 
 class LinkedList:
+
     def __init__(self, value=None):
         self.head_node = Node(value)
 
@@ -22,8 +24,8 @@ class LinkedList:
     def insert_beginning(self, new_value):
         new_node = Node(new_value)
         new_node.set_next_node(self.head_node)
-        
         self.head_node = new_node
+    
     def stringify_list(self):
         string_list = ''
         current_node = self.head_node
@@ -31,8 +33,6 @@ class LinkedList:
         while current_node.get_next_node() != None:
             string_list += f'{current_node.get_value()}\n'
             current_node = current_node.get_next_node()
-            
-        
         return string_list
     
     def remove_node(self, value_to_remove):
